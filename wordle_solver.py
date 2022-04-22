3b3d4b9717fb7df6376b87398cc5ed4d5ceaed52
sdu2030 <86030593+sdu2030@users.noreply.github.com>
2022-04-22 16:15:56 -0400

    Update wordle_solver.py
    - Added increased functionality for yellow letters
    - Added gray word exclusion feature

--- wordle_solver.py
+++ wordle_solver.py
@@ -43,10 +43,13 @@ def specificLetterWords(letterList):
             
       
 def unspecificLetterWords(letterList):
-   letterList = list(letterList)   
-   
+   letterList = list(letterList)
    potentialWords = []
+   valid = 0
    
+   for x in letterList:
+      if x.isalpha():
+         valid += 1
    
    for words in wordles[0]["words"]:
       
@@ -60,30 +63,74 @@ def unspecificLetterWords(letterList):
             contains += 1
             
             # Removes checked letters to prevent the program from mislabeling a word as valid even if it doesn't contain enough duplicate letters
-            tempWord = tempWord[0:tempWord.index(LLletters)]+tempWord[(tempWord.index(LLletters)+1):]
+            tempWord = tempWord[0:tempWord.index(LLletters)] + tempWord[(tempWord.index(LLletters)+1):]
       
-      if contains == len(letterList):
+      if contains == valid:
          potentialWords.append(words)
-
+   
+   #
+   for words in potentialWords:
+      contains = 0
+      
+      # Checks if each letter of a word corresponds to the same spot as the specified input
+      counter = 0
+      while counter < len(letterList):
+         if letterList[counter] == words[counter]:
+            contains += 1
+         
+         counter += 1
+      
+      if contains > 0:
+         potentialWords.remove(words)
+   
    return potentialWords          
+
+def invalidLetterChecker(potentialWords, letterList):
+   letterList = list(letterList)
+   
+   counter = 0
+   while counter < len(potentialWords):
+      contains = 0
+      
+      counter1 = 0
+      while counter1 < len(letterList):
+         if letterList[counter1] in potentialWords[counter]:
+            contains += 1
+         counter1 += 1
+      
+      if contains > 0:
+         potentialWords.remove(potentialWords[counter])
+         counter -= 1
+      
+      counter += 1
+   
+   return potentialWords      
+         
+         
+
       
 
-
-
-# Accepting input:
-
-userGuess0 = input("Please enter unspecific known letters in any order: ").lower()
-userGuess1 = input("Please enter known letters, with spaces: ").lower()
-
-unspecific = unspecificLetterWords(userGuess0)
-specific = specificLetterWords(userGuess1)
-
-#print(unspecific)
-#print(specific)
-
-matches = list(set(specific).intersection(unspecific))
-print()
-print(str(len(matches)) + " satisfactory words found.")
-print(matches)
-
-exit = input("\n\n[Enter] to exit.")
+while True:
+   # Accepting input:
+   
+   userGuess0 = input("Please enter yellow known letters, with spaces: ").lower()
+   userGuess1 = input("Please enter green known letters, with spaces: ").lower()
+   userGuess2 = input("Please enter invalid gray letters, in any order: ").lower()
+   
+   unspecific = unspecificLetterWords(userGuess0)
+   specific = specificLetterWords(userGuess1)
+   
+   #print(unspecific)
+   #print(specific)
+   
+   matches = list(set(specific).intersection(unspecific))
+   matches = invalidLetterChecker(matches, userGuess2)
+   
+   print()
+   print(str(len(matches)) + " satisfactory words found.")
+   
+   print(matches)
+   
+   exit = input("\n\nEnter [Q] to exit.\n").lower()
+   if exit == "q":
+      break
